c19a6fb592f35a3a4e2ec1ed6ca39794260fdcc4
Michael Grimsley <24793858+chillithechillinchinchilla@users.noreply.github.com>
2022-03-08 07:57:57 -0500

    Update game.js
    -Added function comments.
    -Removed debug lines.

--- game.js
+++ game.js
@@ -1,47 +1,45 @@
+// This game asks users to remember a random sequence of buttons which flash and play sound.
+// Once user selects the start button, the game starts with one pattern and continuously adds
+// to the pattern each time the user completes the level.
+// Answer is automatiacally checked when player enters the same amount of buttons as the current gamePattern.
+// Player may ask for a hint which goes through the gamePattern with a delay between each item.
+// Arrays, timers, recursive function, audio functions, jQuery animations and css attribute changes.
+
+
+
 // Init Vars; button color choices, and blank game pattern array.
 var buttonColors = ["red","blue","green","yellow"];
 
-var gamePattern = [];
-var userClickedPattern = [];
+var gamePattern = []; //gamePattern used by game.
+var userClickedPattern = []; // Pattern user enters, is set to 0 at each level.
 
 var level = 0;
 
+//this loads each audio file soplaying is smoother.
 var audio = new Audio("./sounds/green.mp3");
 audio = new Audio("./sounds/red.mp3");
 audio = new Audio("./sounds/blue.mp3");
 audio = new Audio("./sounds/yellow.mp3");
 
 
-//Look for press of start button to init game.
-$(".start").click(function(){
+
+$(".start").click(function(){ //Look for press of start button to init game.
   animateButton("start");
   nextSequence();
   $("#start").text("Michael Says:");
 });
 
-//Look for hint button to play through entire game sequence.
-$(".hint").click(function(){
+$(".hint").click(function(){ //Look for hint button to play through entire game sequence
   animateButton("hint");
   var i = 0;
   hintLoop(i);
 });
 
-function hintLoop(i){ //must init i beforehand
-  setTimeout(function (){
-    color = gamePattern[i];
-    playSound(color);
-    animateButton(color);
-    i++;
-    if (i < gamePattern.length){
-      hintLoop(i);
-    }
-  },1000);//end timeout
-}//end funct
-
-// Look for user button clicks and determin which button was selected.
-$(".btn").click(function(){
-   userChosenColor = $(this).attr("id");
-   userClickedPattern.push(userChosenColor);
+// Look for the user to click a button.
+// Automatically checks the answer once userClickedPattern length = gamePattern length.
+$(".btn").click(function(){ // Look for user button clicks and determine which button was selected.
+   userChosenColor = $(this).attr("id"); // find the name of the button which triggered the event.
+   userClickedPattern.push(userChosenColor); //push color onto answer array.
 
    playSound(userChosenColor);
    animatePress(userChosenColor);
@@ -52,7 +50,7 @@ $(".btn").click(function(){
 });
 
 
-function nextSequence(){ // add to the sequence
+function nextSequence(){ // Adds a random color to the gamePattern.
   randomNumber = Math.floor(Math.random()*4);
   randomChosenColor = buttonColors[randomNumber]; //assign color based on number
   gamePattern.push(randomChosenColor);
@@ -65,20 +63,17 @@ function nextSequence(){ // add to the sequence
   $("h1").text("Level " + level);
 }
 
-function checkAnswer(currentLevel){
+function checkAnswer(currentLevel){ //Check the answer given by player.
   for (var i = 0; i < currentLevel; i++)
     {
-        // console.log("game is: " + gamePattern[i]);
-        // console.log("user is: " + userClickedPattern[i]);
         if (gamePattern[i] === userClickedPattern[i])
         {
-          console.log("good");
           continue;
         }
         else
         {
-        resetGame();
-        return;
+          resetGame();
+          return;
         }
     }
     console.log("You Think you're good eh? How bout deez apples?");
@@ -87,7 +82,7 @@ function checkAnswer(currentLevel){
     }, 1000);
 }
 
-function resetGame(){
+function resetGame(){ // Called when checkAnswer fails. Allows user to restart game.
     console.log("GameOver");
     level = 0;
     gamePattern = [];
@@ -95,11 +90,11 @@ function resetGame(){
     $("#start").text("Click to try again.");
 }
 
-function animateButton(name){ // Animate the button in gamePattern and play the respective sound.
+function animateButton(name){ // Animate button in gamePattern and play the respective sound.
   $("#" + name).fadeIn(100).fadeOut(100).fadeIn(100);
 }
 
-function animatePress(color){ // Animate when user clicks button
+function animatePress(color){ // Animate when user clicks button via addClass. Remove after timeout.
   $("#" + color).addClass("pressed");
   setTimeout(function(){$("#"+ color).removeClass("pressed")},100 );
 }
@@ -107,4 +102,20 @@ function animatePress(color){ // Animate when user clicks button
 function playSound(name){ // Pass in the color you want played.
   var sound = new Audio("sounds/" + name + ".mp3");
   sound.play();
+}
+
+// Recursive function that iterates through gamePattern[]. plays each sound while animating
+// the respective button. Allows user to remember longer sequences.
+// We use timeout function so there is a delay between audio elements.
+// Must pass in the array start i = 0 to start at beggining of gamePattern[]
+function hintLoop(i) {
+    setTimeout(function (){
+      color = gamePattern[i];
+      playSound(color);
+      animateButton(color);
+      i++;
+      if (i < gamePattern.length){
+        hintLoop(i);
+      }
+    },1000); // End Timeout Function.
 }
